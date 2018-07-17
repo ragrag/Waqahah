0334f70c98683dfdffb7f6528ff197589203ac2a
ragrag <raggi144711@bue.edu.eg>
2018-07-17 10:19:20 +0200

    a

--- drops/urls.py
+++ drops/urls.py
@@ -69,3 +69,6 @@ urlpatterns = format_suffix_patterns(urlpatterns)
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_URL)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else 
+	urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_URL)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
