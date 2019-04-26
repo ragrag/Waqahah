c2e8ce2869a5cbd5b1afeaa1499c7ea283ecf4a1
ragrag <raggi144711@bue.edu.eg>
2019-04-27 01:17:22 +0200

    exra

--- drops/settings.py
+++ drops/settings.py
@@ -132,14 +132,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'media'),
+)
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
