ba7a22717183b9cd9c0d89888e68d63add8cfc0f
ragrag <raggi144711@bue.edu.eg>
2019-04-27 01:27:06 +0200

    2

--- drops/settings.py
+++ drops/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/media/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
